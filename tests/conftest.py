7e7b8442c5d3591cc6d1852a462f6e70d3acb42d
DmitryAsl <aslamovdima10@yandex.ru>
2025-02-09 13:26:16 +0300

    add mini fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from selenium import webdriver
 from selene import browser
-#
-@pytest.fixture(params=[(1920, 1080), (1366, 768), (390, 844), (360 ,800)], ids=['Full HD', 'HD', 'iPhone 13/14/15', 'Samsung Galaxy S21'])
+
+
+@pytest.fixture(params=[(1920, 1080), (1366, 768), (390, 844), (360, 800)],
+                ids=['Full HD', 'HD', 'iPhone 13/14/15', 'Samsung Galaxy S21'])
 def browser_config(request):
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
@@ -15,7 +17,8 @@ def browser_config(request):
 
     browser.quit()
 
-@pytest.fixture(params=[(390, 844), (360 ,800)], ids=['iPhone 13/14/15', 'Samsung Galaxy S21'])
+
+@pytest.fixture(params=[(390, 844), (360, 800)], ids=['iPhone 13/14/15', 'Samsung Galaxy S21'])
 def browser_config_mobile(request):
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
@@ -27,6 +30,7 @@ def browser_config_mobile(request):
     yield
 
     browser.quit()
+
 
 @pytest.fixture(params=[(1920, 1080), (1366, 768)], ids=['Full HD', 'HD'])
 def browser_config_desktop(request):
